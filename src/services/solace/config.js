081cad0520ec2d06402763055ac192b0b9622f67
Halal-Face <Zahin.Mohammad@gmail.com>
2019-02-09 13:08:21 -0500

    firebase config

---
+++ src/services/solace/config.js
@@ -0,0 +1,11 @@
+var session = solace.SolclientFactory.createSession({ 
+    url: "wss://mr4b11zr7nh.messaging.mymaas.net:20356", 
+    vpnName: "msgvpn-4b11zr8db", 
+    userName: "solace-cloud-client", 
+    password: "itolh8t940ciughi0h305dv38g", 
+ }); 
+ try { 
+    session.connect(); 
+ } catch (error) { 
+    console.log(error); 
+ }
